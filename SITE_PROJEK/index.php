55cd46dc3b636b52d054fb6321fe018453c1e3ca
phsalvador <rodolphosalvador91@gmail.com>
2017-06-07 16:26:12 -0300

    comitando
    comitando

---
+++ SITE_PROJEK/index.php
@@ -0,0 +1,46 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Projek</title>
+<link rel="stylesheet" type="text/css" href="css/style_menu.css">
+</head>
+<body bgcolor="#ecf0f1">
+	<header class="topo">
+   	  <h3 align="center"><img src="img/logo_projek_final_2017-Mobile.png" alt="Projek"></h3>
+</header>
+<div class="menu">
+  <ul>
+    	<li><a href="#">Negócios</a>
+        	<ul>
+            	<li><a href="index1.html">Análise de Mercado(FORM)</a></li>
+                <li><a href="#">Oportunidade</a></li>
+                <li><a href="#">Prospecção</a>
+                	<ul>
+                    	<li><a href="https://gsuite.google.com.br/intl/pt-BR/products/calendar/">Google Agenda</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Contrato</a></li>                	
+            </ul>
+        </li> 
+        <li><a href="#">Financeiro</a>
+        	<ul>
+            	<li><a href="https://www.contaazul.com/nova-home/">contaazul</a></li>
+        	</ul>        
+        </li> 	
+        <li><a href="#">Compra</a>
+        	<ul>
+            	<li><a href="#">Formulário de Liberação de Compra</a></li>
+        	</ul>        
+        </li>
+        <li><a href="#">Projeto</a>
+        	<ul>
+            	<li><a href="#"></a></li>
+        	</ul>
+        </li>
+</ul>
+</div>
+</body>
+</html>
---
+++ login/index.php
@@ -0,0 +1,28 @@
+﻿<?php
+ini_set('display_erros',0);
+error_reporting(0);
+?>	
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Projek</title>
+<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="css/style.css"> 
+</head>
+<body>
+	<div id="Cadastrar"><a href="/cadastro.php" title="Cadastre-se e faça parte da nossa equipe!">Cadastre-se &raquo;</div>
+	<div id="login" class="bradius">
+    	<div class="message"></div>
+        <div class="logo"><a href="<?php echo $home;?>" title="<?php echo $title;?>"><img src="css/img/logo.png" alt="<?php echo $title;?>" title="<?php echo $title;?> width="250" height="76"></div>
+        <div class="acomodar">
+        <form action="" method="post">
+        <label for="email">E-mail:</label><input id="email "type="text" class="txt bradius" name="email" value=""/>
+        <label for="senha">Senha:</label><input id="senha" type="password" class="txt bradius" name="senha" value=""/>
+        <input type="submit" class="sb bradius" value="Entrar"/>
+        </form>
+        
+        </div>
+    </div>
+</body>
+</html>
